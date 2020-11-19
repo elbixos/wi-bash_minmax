00099f6b7eeafe0870e09d8f40cb5df125111299
elbixos <vpage.vincent@gmail.com>
2020-11-19 16:13:29 -0400

    eval vite fait

--- test_eval.py
+++ test_eval.py
@@ -1,8 +1,100 @@
+
+def factorielle(n):
+    if n <=1:
+        return 1
+
+    return n * factorielle(n-1)
+
+def compteAllumettes(tab) :
+	s = 0
+	for i in tab :
+		s += i
+	return s
+
+def defineCoupsPossibles(plateau):
+    coups = []
+
+    for ligne in range(4):
+        for nb in range(1,4):
+            if nb <= plateau[ligne]:
+                coups.append((ligne,nb))
+    return coups
+
+
 def evaluer(plateau, IA):
-    print ("J'evalue :", plateau, IA)
+    #print ("J'evalue :", plateau, IA)
+
+    if compteAllumettes(plateau) == 0:
+        if IA :
+            return 1
+        return -1
+
+    coups = defineCoupsPossibles(plateau)
+    #print(coups)
+
+    if IA :
+
+        best = -1000
+        for c in coups:
+            ligne = c[0]
+            nb = c[1]
+
+            plateau[ligne]-= nb
+            #print(plateau)
+            resu = evaluer(plateau, not IA)
+            if resu > best :
+                best = resu
+
+            plateau[ligne]+=nb
+    else :
+        best = 1000
+        for c in coups:
+            ligne = c[0]
+            nb = c[1]
+
+            plateau[ligne]-= nb
+            #print(plateau)
+            resu = evaluer(plateau, not IA)
+            if resu < best :
+                best = resu
+
+            plateau[ligne]+=nb
 
 
-plateau = [2,1,0,0]
-IA = True
+    #print ("Je viens d'evaluer :", plateau, IA,":",best)
 
-evaluer(plateau,IA)
+    return best
+
+def choisirIA(plateau):
+    coups = defineCoupsPossibles(plateau)
+
+    best = -1000
+    bestStrategy = None
+    for c in coups:
+        ligne = c[0]
+        nb = c[1]
+
+        plateau[ligne]-= nb
+        #print(plateau)
+        resu = evaluer(plateau, False)
+        if resu > best :
+            best = resu
+            bestStrategy = c
+
+        plateau[ligne]+=nb
+
+    if best>0 :
+        print("Je vais te ni///er en jouant ",bestStrategy)
+    else :
+        print ("joue bien sinon, je vais te....")
+
+    return bestStrategy
+
+
+plateau = [2,2,1,0]
+print(plateau)
+#resu = evaluer(plateau,IA)
+#print ("Pg principal : Evaluation de ",plateau,IA,":", resu)
+
+choix = choisirIA(plateau)
+print("l'IA joue ",choix)
